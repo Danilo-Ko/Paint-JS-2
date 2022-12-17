53239de49cce3141598647ca97869c1a81a0fbb6
Danilo-Ko <danilosrbija@gmail.com>
2022-12-17 12:35:02 +0100

    Color 추가

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+const colorPaint = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
 let isPainting = false;
 
-function onMove (event){
+function onMove (e){
     if(isPainting) {
-        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         return;
     }
-    ctx.moveTo(event.offsetX, event.offsetY);
+    ctx.moveTo(e.offsetX, e.offsetY);
 }
 function onMouseDown (){
     isPainting = true;
@@ -20,7 +22,12 @@ function onMouseDown (){
 function onMouseUp (){
     isPainting = false;
 }
+function onColorPaint (e) {
+       ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.fillStyle = color;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 
+colorPaint.addEventListener("change", onColorPaint);
